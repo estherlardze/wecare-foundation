721475a82b23d9b54b10d9021e5e313f59efed87
estherlardze <estherlardze8@gmail.com>
2024-07-01 20:44:20 +0000

    fix responsiveness issue

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const Header = () => {
   return (
     <div className="bg-hero bg-cover bg-no-repeat bg-center mt-[60px]  h-[60vh] sm:h-[100vh] w-full 2xl:h-[60vh] bg-fixed">
-      <div className="bg-black/50 h-[60vh] sm:h-[100vh] text-white 2xl:h-[60vh] gap-4 w-full">
-        <div className="w-[86%] mx-[7%] flex flex-col items-center justify-center h-[100vh]">
-          <h1 className="font-bold text-xl sm:text-2xl mb-4">
+      <div className="bg-black/50 h-[60vh] sm:h-[100vh] w-full 2xl:h-[60vh] text-white gap-4">
+        <div className="w-[86%] mx-[7%] flex flex-col items-center justify-center h-[60vh] sm:h-[100vh] 2xl:h-[60vh]">
+          <h1 className="font-bold text-2xl sm:text-3xl text-center mb-4">
             We are creating a brighter tomorrow
           </h1>
-          <p className="text-2xl sm:text-3xl md:text-6xl text-center font-bold my-6">
+          <p className="text-3xl sm:text-4xl md:text-6xl text-center font-bold my-6">
             Make someones life by giving of yours
           </p>
         </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -65,19 +65,19 @@ const Navbar = () => {
 
       <div className={`fixed top-0 left-0 w-[80%] min-h-screen bg-white transition-transform duration-500 ease-in-out ${menu ? 'translate-x-0' : '-translate-x-full'} md:hidden z-10`}>
         <div className="flex flex-col items-start justify-center p-6 gap-y-4 pt-12">
-          <Link href="/" className="font-semibold cursor-pointer w-full border-b border-gray-300" onClick={handleMenu}>
+          <Link href="/" className="font-semibold cursor-pointer my-2 w-full border-b border-gray-300" onClick={handleMenu}>
             Home
           </Link>
-          <Link href="/about-us" className="font-semibold w-full cursor-pointer border-b border-gray-300" onClick={handleMenu}>
+          <Link href="/about-us" className="font-semibold w-full my-2 cursor-pointer border-b border-gray-300" onClick={handleMenu}>
             About us
           </Link>
-          <Link href="/projects" className="font-semibold cursor-pointer w-full border-b border-gray-300" onClick={handleMenu}>
+          <Link href="/projects" className="font-semibold cursor-pointer my-2 w-full border-b border-gray-300" onClick={handleMenu}>
             Projects
           </Link>
-          <Link href="/volunteer" className="font-semibold cursor-pointer w-full border-b border-gray-300" onClick={handleMenu}>
+          <Link href="/volunteer" className="font-semibold cursor-pointer my-2 w-full border-b border-gray-300" onClick={handleMenu}>
             Volunteer
           </Link>
-          <Link href="/contact" className="font-semibold cursor-pointer w-full border-b border-gray-300" onClick={handleMenu}>
+          <Link href="/contact" className="font-semibold cursor-pointer my-2 w-full border-b border-gray-300" onClick={handleMenu}>
             Contact
           </Link>
           <Link
